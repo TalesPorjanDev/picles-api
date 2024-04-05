58dfdc3c046d03391565f33e29523d903de5ab25
TalesPorjan <tales.fernandodev@gmail.com>
2024-04-04 22:33:51 -0300

    feat: criado conexão com o banco de dados mongoDB

---
+++ src/shelter/shelter.repository.ts
@@ -0,0 +1,17 @@
+import { Model } from "mongoose";
+import { Shelter } from "./schemas/shelter.schema";
+import { InjectModel } from "@nestjs/mongoose";
+import IShelterRepository from "./interfaces/shelter.repository.interface";
+
+export class ShelterRepository implements IShelterRepository{
+    constructor(
+        @InjectModel(Shelter.name)
+        private readonly shelterModel: Model<Shelter>
+    ){}
+
+    async get(): Promise<Shelter> {
+        return await this.shelterModel.findOne()
+    }
+}
+
+
