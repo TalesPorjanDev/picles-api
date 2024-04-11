0c8cd0b6c714d2f4a5a8018533912c061f3b5221
TalesPorjan <tales.fernandodev@gmail.com>
2024-04-11 19:48:28 -0300

    foi adicionado um path de upload de foto no modulo e foi adicionado uma foto de teste

--- src/pet/pet.controller.ts
+++ src/pet/pet.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Body, Controller, Delete, Get, Inject, Param, Patch, Post, Put } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, Inject, Param, Patch, Post, Put, UploadedFile, UseInterceptors } from '@nestjs/common';
 import CreatePetControllerInput from './dtos/create.pet.controller.input';
 import PetTokens from './pet.tokens';
 import { IuseCase } from 'src/domain/iusecase.interface';
@@ -11,6 +11,8 @@ import UpdatePetByIdUseCaseInput from './usecases/dtos/update.pet.by.id.usecase.
 import UpdatePetByIdUseCaseOutput from './usecases/dtos/update.pet.by.id.usecase.output';
 import DeletePetByIdUseCaseInput from './usecases/dtos/delete.pet.by.id.usecase.input';
 import DeletePetByIdUseCaseOutput from './usecases/dtos/delete.pet.by.id.usecase.output';
+import { FileInterceptor } from '@nestjs/platform-express';
+import multerConfig from 'src/config/multer.config';
 
 @Controller('pet')
 export class PetController {
@@ -65,6 +67,15 @@ export class PetController {
         } catch (error) {
             throw new BadRequestException(JSON.parse(error.message))
         }
+    }
+
+    @Patch(':id/photo')
+    @UseInterceptors(FileInterceptor('photo', multerConfig))
+    updatePhoto(
+        @UploadedFile() photo: Express.Multer.File,
+        @Param('id') id: string,
+    ) {
+        console.log(photo, id)
     }
 
 }
