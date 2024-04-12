c30713ae80a765825090e1c776e66c53408c97ba
TalesPorjan <tales.fernandodev@gmail.com>
2024-04-12 18:37:14 -0300

    feat: foi retirado um arquivo pet repository que estava duplicado

--- src/pet/interfaces/pet.repository,interface.ts
+++
@@ -1,8 +0,0 @@
-import { Pet } from "../schema/schema";
-
-export default interface IPetRepository {
-    create(data: Partial<Pet>): Promise<Pet>
-    getById(id: string): Promise<Pet>
-    updateById(data: Partial<Pet>): Promise<void>
-    deleteById(id: string): Promise<void>
-}
